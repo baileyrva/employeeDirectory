db71a483c56fc52cca1c4a088c492ef38b190b38
Joe Bailey <baileyrva@gmail.com>
2020-03-26 16:03:27 -0400

    title js and styling pages

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Title.css";
+
+function Title() {
+  return (
+    <header className="title">
+      <h1>Employee Directory</h1>
+    </header>
+  );
+}
+
+export default Title; 
