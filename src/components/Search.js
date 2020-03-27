6353de3103cf6291ef8ef96247737a26545e6959
Joe Bailey <baileyrva@gmail.com>
2020-03-27 15:46:58 -0400

    grainular component setup

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,23 +1,31 @@
-import React from "react";
-import "../styles/Search.css";
+import React, { useState, useContext } from "react";
+import { EmployeeContext } from "./EmployeeContext.js";
 
-function Search(props) {
-  return (
-    <form onSubmit={e => e.preventDefault()}>
-      <div className="form-group">
-        <label htmlFor="search">Search:</label>
-        <input
-          onChange={props.handleInputChange}
-          value={props.search}
-          name="search"
-          type="text"
-          className="form-control"
-          placeholder="Employee search"
-          id="search"
-        />
-      </div>
-    </form>
-  );
+
+function Search() {
+  // stores the characters being entered into the search input
+  const [search, setSearch] = useState("");
+  const { employees, setDisplayedEmployees } = useContext(EmployeeContext);
+
+  function updateSearch({ target }) {
+    // just using for readability
+    const searchTerm = target.value;
+    // used for updating the value in the search input, so it always equals exactly what is saved in state
+    setSearch(searchTerm);
+    // filters the employees based on the text that is entered in the search input
+    const filterResult = employees.filter(function(employee) {
+      // Looks at every employee first name, makes the name lowercase,
+      // ... if first name contains the current search, display
+      // ... if not it is removed from the list.
+      return employee.name.first
+        .toLowerCase()
+        .indexOf(searchTerm.toLowerCase()) !== -1
+        ? true
+        : false;
+    });
+    setDisplayedEmployees([...filterResult]);
+  }
+  // every letter entered triggers the onChange event.
+  return <input type="text" onChange={updateSearch} value={search}></input>;
 }
-
-export default Search; 
+export default Search;
